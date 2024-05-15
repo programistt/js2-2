b14cb59d4390647ed09f4af97ad6417dd7236633
Islom Toirjonov <toirjonovislomjon3@gmail.com>
2024-05-15 11:50:31 +0500

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+let ism = prompt("ismingizni kiriting :");
+let id = prompt("id kiriting :");
+let pass = prompt("passwordingizni kiriting :")
+
+let data = {
+    name: ism,
+    id: id,
+    parol: pass
+};
+
+console.log(data);
